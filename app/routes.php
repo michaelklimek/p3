387e91f01bb5b8afac411cbfa4456073ab804a41
Michael Klimek <michaelklimek@g.harvard.edu>
2014-10-19 22:32:04 -0500

    "Built Routes"

--- app/routes.php
+++ app/routes.php
@@ -9,9 +9,36 @@
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the Closure to execute when that URI is requested.
 |
-*/
+
 
 Route::get('/', function()
 {
 	return View::make('hello');
+});
+
+*/
+
+Route::get('/', function() 
+{
+	return 'Developers Best Friend';
+});
+
+Route::post('/paragraph', function()
+{
+	return 'Paragraph Generator';
+});
+
+Route::get('/paragraph/result', function() 
+{
+	return 'Paragraph Results';
+});
+
+Route::post('/user', function()
+{
+	return 'User Generator';
+});
+
+Route::get('/user/result', function()
+{
+	return 'User Results';
 });
