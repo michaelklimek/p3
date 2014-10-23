69acde11133af67a45c5bd891b61ea37e89c9add
Michael Klimek <michaelklimek@g.harvard.edu>
2014-10-23 02:15:18 -0500

    Starting work on User Generator

--- app/views/user.blade.php
+++ app/views/user.blade.php
@@ -11,8 +11,17 @@
 		'url' => 'user',
 		'method' => 'GET')) }}
 		{{ Form::label('number_user', 'Number of Users to Generate (Max 99): ') }}
-		{{ Form::text('users') }}
+		{{ Form::number('users', '', array('min' => '1', 'max' => 99)) }}
 		{{ Form::submit('Generate') }}
 	{{ Form::close() }}
+
+<?php
+	require_once '/path/to/Faker/src/autoload.php';
+	$query = Input::get('users');
+
+	for ($i=0; $i < $query; $i++) {
+ 		echo $faker->name, "\n";
+	}
+?>
 
 @stop
