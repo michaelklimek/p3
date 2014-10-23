1174008b76b0015ee07c58a229bea9dfcb9ce978
Michael Klimek <michaelklimek@g.harvard.edu>
2014-10-22 23:59:10 -0500

    Added a description of the applicaiton

--- app/views/_master.blade.php
+++ app/views/_master.blade.php
@@ -8,6 +8,10 @@
 </head>
 <body>
 	<h1>P3 - Paragraph and User Generators</h1>
+	<p>The application produces paragraphs or random users depending on the quantity requested.
+		The paragraphs are written in Latin in a text called Lorem Ipsum. It is used to divert attention 
+		away from the content of a website, so that the focus is on the design and structure. The User Generator displays random users for 
+		similar purposes to the Paragraph Generator.</p>
 	<a href="/">Home</a>
 	<a href="/paragraph">Paragraph Generator</a>
 	<a href="/user">User Generator</a>
