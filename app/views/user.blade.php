2907cfb07861c9c6883969aad2e1f9b0f8dde491
Michael Klimek <michaelklimek@g.harvard.edu>
2014-10-22 23:30:13 -0500

    Updated form to GET method

--- app/views/user.blade.php
+++ app/views/user.blade.php
@@ -7,7 +7,9 @@
 @section('body')
 	<h1>User Generator</h1>
 	<p>Select how many users you would like to generate</p>
-	{{ Form::open(array('url' => 'user')) }}
+	{{ Form::open(array(
+		'url' => 'user',
+		'method' => 'GET')) }}
 		{{ Form::label('number_user', 'Number of Users to Generate (Max 99): ') }}
 		{{ Form::submit('Generate') }}
 	{{ Form::close() }}
